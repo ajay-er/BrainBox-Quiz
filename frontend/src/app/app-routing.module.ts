f3093d10a155b4bea79863196d94a19ae032806c
ajay <ajayzyan100@gmail.com>
2023-11-06 17:22:48 +0530

    login added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './shared/ui/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/feature/auth-shell/auth-shell.module').then(
+        (m) => m.AuthShellModule
+      ),
+    // canActivate: [unauthenticatedGuard],
+  },
+  // {
+  //   path:'home',
+
+  // },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ frontend/src/app/auth/feature/auth-container/auth-container.component.ts
@@ -0,0 +1,10 @@
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-container',
+  templateUrl: './auth-container.component.html',
+  styleUrls: ['./auth-container.component.css'],
+})
+export class AuthContainerComponent {
+  login(){}
+}
