c3db39c4c87610b3dc51d1e475908ac309f4444a
ajay <ajayzyan100@gmail.com>
2023-11-06 20:10:16 +0530

    responsive signup page

--- frontend/src/app/auth/ui/login/login.module.ts
+++ frontend/src/app/auth/ui/login/login.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoadingButtonModule } from 'src/app/shared/ui/loading-button/loading-button.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, ReactiveFormsModule, LoadingButtonModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    LoadingButtonModule,
+  ],
   exports: [LoginComponent],
 })
 export class LoginModule {}
