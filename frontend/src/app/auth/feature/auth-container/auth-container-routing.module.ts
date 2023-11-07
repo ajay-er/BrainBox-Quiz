1967988a7a729423b70c4b4b5a25186c5ba1d53b
ajay <ajayzyan100@gmail.com>
2023-11-08 01:28:56 +0530

    small issues resolved

--- frontend/src/app/auth/feature/auth-container/auth-container-routing.module.ts
+++ frontend/src/app/auth/feature/auth-container/auth-container-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthContainerComponent } from './auth-container.component';
+import { LoginComponent } from '../../ui/login/login.component';
+import { SignupComponent } from '../../ui/signup/signup.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AuthContainerComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+    ],
   },
 ];
 
