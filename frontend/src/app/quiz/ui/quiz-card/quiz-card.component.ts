4fd609eacff7569bb18e0dddef479d6f0dc9bbf1
ajay <ajayzyan100@gmail.com>
2023-11-10 03:31:46 +0530

    quiz card completed

---
+++ frontend/src/app/quiz/ui/quiz-card/quiz-card.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-card',
+  templateUrl: './quiz-card.component.html',
+  styleUrls: ['./quiz-card.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class QuizCardComponent {
+
+}
