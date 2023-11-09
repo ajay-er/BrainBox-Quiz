5509fb6f9fd82b3ac42855f84fc3a7818bbb1ca2
ajay <ajayzyan100@gmail.com>
2023-11-10 03:31:46 +0530

    quiz card completed

--- frontend/src/app/quiz/feature/quiz-container/quiz-container.module.ts
+++ frontend/src/app/quiz/feature/quiz-container/quiz-container.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { QuizContainerRoutingModule } from './quiz-container-routing.module';
 import { QuizContainerComponent } from './quiz-container.component';
+import { QuizCardModule } from '../../ui/quiz-card/quiz-card.module';
 
 @NgModule({
   declarations: [QuizContainerComponent],
-  imports: [CommonModule, QuizContainerRoutingModule],
+  imports: [CommonModule, QuizContainerRoutingModule,QuizCardModule],
 })
 export class QuizContainerModule {}
