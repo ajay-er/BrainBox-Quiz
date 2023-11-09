89b8fd5c913ba3f8eceb6fe114830edfe300db3c
ajay <ajayzyan100@gmail.com>
2023-11-10 03:31:46 +0530

    quiz card completed

---
+++ frontend/src/app/quiz/ui/quiz-result/quiz-result.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-result',
+  templateUrl: './quiz-result.component.html',
+  styleUrls: ['./quiz-result.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class QuizResultComponent {
+
+}
---
+++ frontend/src/app/quiz/ui/quiz-result/quiz-result.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QuizResultComponent } from './quiz-result.component';
+
+@NgModule({
+  declarations: [QuizResultComponent],
+  imports: [CommonModule],
+  exports: [QuizResultComponent],
+})
+export class QuizResultModule {}
